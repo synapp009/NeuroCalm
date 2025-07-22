887d265c9d51bd8bf808d897069fbdf2ab89c39f
Michael Berger <michael.andreas.berger@outlook.com>
2025-07-22 12:22:09 +0100

    change versionCode and Name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "nodomain.synapp.neurocalm"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
