f0cad1af10d56b4ddf4ed067925ceb6cf1785789
Michael Berger <michael.andreas.berger@outlook.com>
2025-08-01 09:34:46 +0100

    update gradle and fastlane

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "nodomain.synapp.neurocalm"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
